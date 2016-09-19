92b206cae0c8ff4cc5202a7ce29a2382d57fb303
k24dizzle <zhaok24@uw.edu>
2016-09-19 10:18:27 -0700

    formatted?

--- file_formatter.py
+++ file_formatter.py
@@ -3,20 +3,54 @@ stocks = open('stocks_copy.txt')
 stock_data = stocks.read().split('\n')
 current_dir = os.path.dirname(__file__)
 print current_dir
-for stock in stock_data:
-    top_name = "%s_Top.csv" % (stock)
-    rising_name = "%s_Rising.csv" % (stock)
-    with open('New_Data/' + top_name, "w") as outfile:
-        outfile.write("Year,Top_Search,Score,Ticker")
-        outfile.write('\n')
+
+with open('New_Data/Top_Data.csv', "w") as outfile:
+    outfile.write("Year,Top_Search,Score,Ticker")
+    outfile.write('\n')
+    for stock in stock_data:
         for year in xrange(2004, 2014):
             get = "%s_%s_top.csv" % (stock, year)
             folder = "Stock_Data/%s/top/" % (stock)
-            with open(folder + get, 'rb') as infile:
-                year_data = infile.read().split('\n')
-                for line in year_data:
-                    if line != '':
-                        outfile.write("%s," % (year))
-                        outfile.write(line)
-                        outfile.write(",%s" % (stock))
-                        outfile.write('\n')
+            try:
+                with open(folder + get, 'rb') as infile:
+                    year_data = infile.read().split('\n')
+                    for line in year_data:
+                        if line != '':
+                            outfile.write("%s," % (year))
+                            outfile.write(line)
+                            outfile.write(",%s" % (stock))
+                            outfile.write('\n')
+            except IOError:
+                print "%s not found" % (get)
+
+with open('New_Data/Rising_Data.csv', "w") as outfile:
+    outfile.write("Year,Rising_Search,Score,Ticker")
+    outfile.write('\n')
+    for stock in stock_data:
+        for year in xrange(2004, 2014):
+            get = "%s_%s_rising.csv" % (stock, year)
+            folder = "Stock_Data/%s/rising/" % (stock)
+            try:
+                with open(folder + get, 'rb') as infile:
+                    year_data = infile.read().split('\n')
+                    for line in year_data:
+                        if line != '':
+                            outfile.write("%s," % (year))
+                            data = line.split(',')
+                            if len(data) > 2:
+                                if data[-2][0] == '+':
+                                    temp = data[-2] + data[-1]
+                                    data[-2] = temp
+                                    del data[-1]
+                                    if len(data) > 2:
+                                        other_temp = ''
+                                        for i in xrange(0, len(data) - 1):
+                                            other_temp += data[i]
+                                        data[0] = other_temp
+                                        data = [data[0], data[-1]]
+
+                            outfile.write(','.join(data))
+                            outfile.write(",%s" % (stock))
+                            outfile.write('\n')
+            except IOError:
+                a =  "%s not found" % (get)
